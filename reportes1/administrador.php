57e0162f28710161f9ca1784d9d8ec12e84c3c70
brahanVeld <brahanVeld@users.noreply.github.com>
2020-12-26 19:52:23 -0600

    ultimos cambios

--- reportes1/administrador.php
+++ reportes1/administrador.php
@@ -7,56 +7,41 @@
  */
 //include 'conexion.php';
 include 'encabezado.php';
+session_start();
 ?>
-<div>
+<!--div class="container">
     
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                 <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-            </button>
-            
-        </div>
-        <div class="navbar-collapse collapse">
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarNav">
         
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Usuarios</a></li>
-                <li><a href="#">Clientes</a></li>
-                <li><a href="#">Reportes</a></li>
+            <ul class="navbar-nav">
+                <li class="nav-item active"><a class="nav-link" href="#">Usuarios</a></li>
+                <li class="nav-item active"><a class="nav-link" href="#">Clientes</a></li>
+                <li class="nav-item active"><a class="nav-link" href="#">Reportes</a></li>
             </ul>
         </div>
     </nav>
+</div-->
+
+
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h5 class="text-white h4">Menú administrador (a)</h5>
+      <h4 class="text-white h4"><?php echo "".$_SESSION['nombre'];?></h4>
+      <span class="text-muted">
+          <ul>
+              <li class="nav-item active"><a class="nav-link" href="adminusuarios.php?id=0">Usuarios</a></li>
+              <li class="nav-item active"><a class="nav-link" href="adminclientes.php?id=0">Clientes</a></li>
+              <li class="nav-item active"><a class="nav-link" href="adminreportes.php?id=0">Reportes</a></li>
+              <li class="nav-item active"><a href="cerrarsesion.php">Salir</a></li>
+          </ul>
+      </span>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
 </div>
-<div>
-    <!----sección para mostrar los usuarios y agregarlos---->
-    <section>
-        <form>
-            <div>
-                <label>Nombre</label>
-                <input type="text" class="login-username" placeholder="Nombre"/>
-            </div>
-            <div>
-                <label>Nombre de usuario</label>
-                <input type="text" class="login-username" placeholder="Nombre de usuario"/>
-            </div>
-            <div>
-                <label>Contraseña</label>
-                <input type="password" class="login-password" />
-            </div>
-            <div>
-                <label>Repetir contraseña</label>
-                <input type="password" class="login-password" />
-            </div>
-            
-        </form>
-        
-    </section>
-</div>
-
-
-
-<?php
-include 'pie.php';
-?>
